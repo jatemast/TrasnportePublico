e4befb1de0a187d8b8679e9874de98a5cf5a5e26
JavierTeheranDev <javicellcartagena@gmail.com>
2024-07-18 23:29:53 -0500

    Mejore la vista index donde se filtran los aldos de targetas y usuario

--- resources/views/compras/index.blade.php
+++ resources/views/compras/index.blade.php
@@ -2,39 +2,40 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Compras') }}
-        </h2>
+
+          
+       
     </x-slot>
 
+    <br>
+    <br>
 
- <br>
- <br>
-
-            <div class="w-screen">
-                <table class="border border-gray-300 rounded-lg mx-auto">
-                    <thead>
-                        <tr>
-                            <th class="border-b border-gray-300">Nombre de usuario</th>
-                            <th class="border-b border-gray-300">Cedula del usuario</th>
-                            <th class="border-b border-gray-300">Codigo de targeta</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($targetas as $targeta)
-                        <tr>
-                            <td class="border-b border-gray-300">{{ $targeta->idTargeta }}</td>
-                            <td class="border-b border-gray-300">{{ $targeta->nombre }}</td>
-                            <td class="border-b border-gray-300">{{ $targeta->cedula }}</td>
-                            <td class="border-b border-gray-300">{{ $targeta->saldo }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+    <div class="w-4/5 mx-auto">
+        <table class="w-full border border-gray-300 rounded-lg">
+            <thead>
+                <tr>
+                    <th class="border-b border-gray-300 text-center">Nombre de usuario</th>
+                    <th class="border-b border-gray-300 text-center">Cedula del usuario</th>
+                    <th class="border-b border-gray-300 text-center">Codigo de tarjeta</th>
+                    <th class="border-b border-gray-300 text-center">Saldo</th>
+                    <th class="border-b border-gray-300 text-center">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($targetas as $targeta)
+                <tr>
+                    <td class="border-b border-gray-300 text-center">{{ $targeta->nombre }}</td>
+                    <td class="border-b border-gray-300 text-center">{{ $targeta->cedula }}</td>
+                    <td class="border-b border-gray-300 text-center">{{ $targeta->idTargeta }}</td>
+                    <td class="border-b border-gray-300 text-center">{{ $targeta->saldo }}</td>
+                    <td class="border-b border-gray-300 text-center">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+                            Actualizar usuario
+                        </button>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-
-
-
-
-
 </x-app-layout>
-
