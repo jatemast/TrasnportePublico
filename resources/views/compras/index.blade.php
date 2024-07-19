3c3d788528df27112ad43917ebebcddad9b87009
javiertm-2023 <programador14@tecnologicocomfenalco.edu.co>
2024-07-19 12:02:52 -0500

    Ya se agrego el boton de filtrado al avista de buscar usuarios sin funcionalidad solo vista

--- resources/views/compras/index.blade.php
+++ resources/views/compras/index.blade.php
@@ -3,12 +3,22 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Compras') }}
 
-          
-       
+
+
     </x-slot>
 
     <br>
     <br>
+    <div class="w-4/5 mx-auto flex justify-end">
+        <form action="">
+            <div class="flex items-center justify-center mb-4">
+                <label for="site-search" class="mr-2">Buscar usuario</label>
+                <input type="search" id="site-search" name="q" class="border border-gray-300 rounded-lg px-2 py-1" />
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">Buscar</button>
+            </div>
+        </form>
+    </div>
+
 
     <div class="w-4/5 mx-auto">
         <table class="w-full border border-gray-300 rounded-lg">
